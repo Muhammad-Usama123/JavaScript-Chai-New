389ee6155e54255e832c47d4f8c5983ff565972a
Muhammad-Usama123 <osamaa.g777@gmail.com>
2023-12-13 00:04:31 +0500

    chai commit after datatypes wednesday starting 13/12/2023 00:04

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JavaScript code as newer version.
+
+// alert(3 + 3) // we are using nodejs, not browser.
+
+// console.log(3 
+//     +
+//     3) // don't write code like this, code readability should be high.
+
+// console.log(3 + 3) // Write code like this.
+
+// Data Types:
+
+// 2 categories of datatypes below:
+
+/*
+1) Primitive Data Types:
+
+number => 2 to power 53
+bigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+*/
+
+/*
+2) Object
+*/
+
+// console.log(typeof null) // Note: Type of null will be shown object in console.
+// console.log(typeof undefined) // undefined 
